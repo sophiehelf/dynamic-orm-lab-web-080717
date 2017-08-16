74769762cf28438905ef52046344da573cbca11f
sophiehelf <sophie.helf@gmail.com>
2017-08-16 13:36:22 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,64 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+  	self.to_s.downcase.pluralize
+  end
+
+  def self.find_by(value)
+  	# if value.values[0].class.is_a?(String)
+  	# 	sql = "SELECT * FROM #{self.table_name} WHERE value = #{"value"}"
+  	# elsif value.values[0].class.is_a?(Fixnum)
+  	# 	sql = "SELECT * FROM #{self.table_name} WHERE value = #{value}"
+  	# end
+  	value_key = value.keys[0]
+  	value_values = value.values[0]
+  	sql = "SELECT * FROM #{table_name} WHERE #{value_key} = ?"
+  	DB[:conn].execute(sql, value_values)
+  end
+
+  def self.column_names
+  	DB[:conn].results_as_hash = true
+  	sql = "PRAGMA table_info('#{table_name}')"
+  	table_info = DB[:conn].execute(sql)
+  	column_names = []
+  	table_info.each do |column|
+  		column_names << column["name"]
+  	end
+  	column_names.compact
+  end
+
+   def initialize(options={})
+  	options.each do |property, value|
+  		self.send("#{property}=", value)
+  	end
+  end
+
+  def table_name_for_insert
+  	self.class.table_name
+  end
+
+  def values_for_insert
+  	values = []
+  	self.class.column_names.each do |col_name|
+  		values << "#{send(col_name)}" unless send(col_name).nil?
+  	end
+  	values.map{ |e| "'#{e}'"}.join(", ")
+  end
+
+  def col_names_for_insert
+  	self.class.column_names.delete_if { |col| col == "id"}.join(", ")
+  end
+
+  def save
+  	sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  	DB[:conn].execute(sql)
+  	@id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+  	sql = "SELECT * FROM #{self.table_name} WHERE name = #{"name"}"
+  	DB[:conn].execute(sql)
+  end
+
 end
